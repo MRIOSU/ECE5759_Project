9a37d2d0562e34ee930ad7dd92372d5f0bddca3b
Chong <30392188+MRIOSU@users.noreply.github.com>
2020-12-03 14:11:10 -0500

    Add files via upload

--- Functions/preprocessing/noise_prewhiting.m
+++ Functions/preprocessing/noise_prewhiting.m
@@ -13,19 +13,29 @@ if (nargin > 1 && ~isempty(param.noise))
     tmp = 1/(size(nScan,2)-1) * (nScan*nScan'); % covriance matrix
     tmp = (tmp + tmp')/2; % To make the matrix complex hermitian
     L = chol(tmp, 'lower');
-    %         figure; imagesc(abs(L)); colobar;
-    L_inv = inv(L);
     data =  permute(data, [4,1:3,5:numel(N)]); % [CH, RO, E1, E2, PHS, SET, SLC]
     data = reshape(data, [size(data,1), numel(data)/size(data,1)]); % [CH, *]
-    data = L_inv * data;
     data = L\data;
     data = reshape(data, N([4,1:3,5:numel(N)])); % [CH, RO, E1, E2, PHS, SET, SLC]
     Y = permute(data, [2:4,1,5:numel(N)]); % [RO, E1, E2, CH, PHS, SET, SLC]
+    
+    % check the noise estimated from the k-space after noise-prewhiting
+    tmpStd = Y([1:min(16,round(size(Y,1)/8)), end-min(16,round(size(Y,1)/8))+1:end],:,:,:,:,:,:,:,:,:);
+    tmpStd = tmpStd(:, [1:round(size(Y,2)/5), end-round(size(Y,2)/5)+1:end],:,:,:,:,:,:,:,:);
+    tmpStd = tmpStd(:, :, [1:max(1,round(size(Y,3)/5)), end-round(size(Y,3)/5)+1:end],:,:,:,:,:,:,:,:);
+    mStd = zeros(size(Y,4),1);    
+    for i = 1:size(Y,4)
+        tmp = tmpStd(:,:,:,i,:,:);  tmp = std(tmp(tmp~=0));
+        mStd(i) = tmp; % Noise in the ith coil
+    end
+    disp('Noise estimate from the k-space after noise-prewhiting:')
+    disp(num2str(mStd'));
+    
 else
     warning('Noise is estimated from the k-space data');
     tmpStd = data([1:min(16,round(size(data,1)/8)), end-min(16,round(size(data,1)/8))+1:end],:,:,:,:,:,:,:,:,:);
     tmpStd = tmpStd(:, [1:round(size(data,2)/5), end-round(size(data,2)/5)+1:end],:,:,:,:,:,:,:,:);
-    tmpStd = tmpStd(:, :, [1:round(size(data,3)/5), end-round(size(data,3)/5)+1:end],:,:,:,:,:,:,:,:);
+    tmpStd = tmpStd(:, :, [1:max(1,round(size(data,3)/5)), end-round(size(data,3)/5)+1:end],:,:,:,:,:,:,:,:);
     mStd = zeros(size(data,4),1);
     Y = zeros(size(data));
     for i = 1:size(data,4)
@@ -34,7 +44,7 @@ else
         mStd(i) = tmp; % Noise in the ith coil
         Y(:,:,:,i,:,:,:) = data(:,:,:,i,:,:,:)/tmp;
     end
-    disp(num2s)
+    disp(num2str(mStd'));
     % tmp = prctile(sort(mStd), 25)/1.2; % pick a coil close to the bottom in terms of signal power
     % Y = data*nStd/tmp;
 end
