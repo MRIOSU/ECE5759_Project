b2fd08b486f4ae324e15eb376deb2a825d87631b
Chong Chen <30392188+MRIOSU@users.noreply.github.com>
2023-03-07 11:51:26 -0500

    make sure the unsampled data zero

--- Functions/preprocessing/remove_RO_oversamling.m
+++ Functions/preprocessing/remove_RO_oversamling.m
@@ -7,6 +7,11 @@ I = fftshift(ifft(ifftshift(data,1),[],1),1)* sqrt(N(1));
 I(1:round(N(1)*fd(1)),:,:,:,:,:,:,:,:,:)=[]; % remove first 1/4
 I(end-round(N(1)*fd(2))+1:end,:,:,:,:,:,:,:,:,:)=[]; % remove last 1/4
 Y = fftshift(fft(ifftshift(I,1),[],1),1)/sqrt(size(I,1));% / sqrt(N(1)/(N(1)-z1-z2));
+RO = size(data,1);
+samp = logical(sum(abs(reshape(data,[RO, numel(data)/RO])),2));
+pre_z = find(samp == 0, 1, 'last' ); % parameter related to assymetry echo
+pre_z = floor(pre_z*(1-sum(fd)));
+Y(1:pre_z,:,:,:,:,:,:,:,:) = 0; %make sure the unsampled data zero
 
 end
 
